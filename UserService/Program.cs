1c358cc658624eab3d83d5bde8e0b90712dc75db
Pragnya Nagure <prnag@eg.dk>
2024-10-07 09:50:44 +0530

    Userservice : clean up

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -13,31 +13,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructureService(builder.Configuration);
-/*
-builder.Services.AddDbContext<UserContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true, // Ensure the token's issuer matches the expected issuer
-        ValidateAudience = true, // Ensure the token's audience matches the expected audience
-        ValidateLifetime = true, // Ensure the token is not expired
-        ValidateIssuerSigningKey = true, // Validate the token's signature with the secret key
-        ValidIssuer = builder.Configuration["Jwt:Issuer"], // Your token issuer
-        ValidAudience = builder.Configuration["Jwt:Audience"], // Your token audience
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])) // Your secret key
-    };
-});
-*/
-
 
 var app = builder.Build();
 
