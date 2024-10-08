8c60539cda79b33c993912da13f5fd2a5eedcfab
Pragnya Nagure <prnag@eg.dk>
2024-10-08 13:17:39 +0530

    Webapp - Sign Up and Sign In | Swagger UI working

---
+++ WebApp/DTOs/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.DTOs
+{
+    public record Response<T>(bool Success = false, string Message = "", T? Data = default, ErrorCode? ErrorCode = null)
+    {
+        // Factory Methods
+        public static Response<T> SuccessResponse(string message, T data) => new Response<T>(true, message, data);
+        public static Response<T> ErrorResponse(string message, ErrorCode? errorCode = null) => new Response<T>(false, message, default, errorCode);
+        public static Response<T> ErrorResponse(Exception exception) => new Response<T>(false, exception.Message);
+    }
+}
