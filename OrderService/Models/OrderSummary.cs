ba17c03cdbc5ff18ec406d2a0a9fd645abfa7efd
Pragnya Nagure <prnag@eg.dk>
2024-10-11 11:00:42 +0530

    order models

--- OrderService/Models/OrderSummary.cs
+++ OrderService/Models/OrderSummary.cs
@@ -1,9 +1,17 @@
-﻿namespace OrderService.Models
+﻿using RestaurantManagement.SharedLibrary.Models;
+
+namespace OrderService.Models
 {
     public class OrderSummary : BaseEntity
     {
-        public decimal TaxAmount { get; set; } = 10;
+        public decimal TaxAmount { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public Guid TableId { get; set; } // Reference to table Id placed
         public int TableNumber { get; set; }
+        public Guid UserId { get; set; } // Reference to the user who placed the order
+
         public List<Order> Orders { get; set; } = new List<Order>(); // List to store multiple orders
         public Order Order { get; set; }
     }
