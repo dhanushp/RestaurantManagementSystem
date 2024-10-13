596eb468f285b357ee0884a12ef0b7bf9db07384
Pragnya Nagure <prnag@eg.dk>
2024-10-13 12:38:56 +0530

    Payment by cash

--- RestaurantManagement.SharedDataLibrary/DTOs/Payment/PaymentDTO.cs
+++ RestaurantManagement.SharedDataLibrary/DTOs/Payment/PaymentDTO.cs
@@ -1,13 +1,14 @@
-﻿using RestaurantManagement.SharedDataLibrary.Enums;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using RestaurantManagement.SharedDataLibrary.Enums;
 
 namespace RestaurantManagement.SharedDataLibrary.DTOs.Payment
 {
-    public class PaymentDTO
+    public class PaymentCashDTO
     {
-        public int OrderId { get; set; }
+        public Guid? UserId { get; set; }
+        public Guid? FoodOrderId { get; set; }
         public decimal Amount { get; set; }
-        public PaymentMethod PaymentMethod { get; set; }
-        public string? TransactionId { get; set; }
+        public string CurrencyCode { get; set; }
     }
 
     public class UpdatePaymentStatusDTO
