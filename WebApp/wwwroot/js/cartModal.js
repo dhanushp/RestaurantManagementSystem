c650be298363468205d27601b6982ed666bb3d6f
dhanush <dhanushp.work@gmail.com>
2024-10-13 21:07:31 +0530

    Fix modal

---
+++ WebApp/wwwroot/js/cartModal.js
@@ -0,0 +1,15 @@
+﻿window.cartModal = {
+    openDialog: function (id) {
+        const dialog = document.getElementById(id);
+        if (dialog) {
+            dialog.showModal();
+        }
+    },
+
+    closeDialog: function (id) {
+        const dialog = document.getElementById(id);
+        if (dialog) {
+            dialog.close();
+        }
+    }
+};
